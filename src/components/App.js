bd9127a3b1dbf426393bf409f6764e0d33e542bd
Lautaro Caraballo <lautaro.caraballo.1922@gmail.com>
2022-12-25 17:39:01 -0300

    inicio de proyecto

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+
+const App = () => {
+  return (
+    <>
+      <header>
+        <h1>Titulo</h1>
+        <nav>
+          <ul>
+            <li><a >Link 1</a></li>
+            <li><a >Link 2</a></li>
+            <li><a >Link 3</a></li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default App;
